6dc3010624c39e4d17c706cdfdb1b6ac15a8ac35
LEE-PABLO <leepaoula01@gmail.com>
2025-12-31 10:22:21 +0900

    12/31-1

---
+++ Practice/src/Prob1.java
@@ -0,0 +1,7 @@
+
+public class Prob1 {
+	public int compute(int num) {
+		if(num<=1) return num;
+		return compute(num-1)+compute(num-3);
+	}
+}
---
+++ Practice/src/Prob1Tester.java
@@ -0,0 +1,9 @@
+
+public class Prob1Tester {
+
+	public static void main(String[] args) {
+		Prob1 p=new Prob1();
+		System.out.println(p.compute(4));
+	}
+
+}
