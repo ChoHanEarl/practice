1dc47e34d4e8a3fc564389a6e64d40d137b1ba7a
jarijarinojari-ui <jarijarinojari@gmail.com>
2026-01-01 01:25:46 +0900

    1231Test01

--- Test01.java
+++ Test01.java
@@ -1,6 +1,6 @@
 package test;
 
-public class Test01 {
+class Test {
 	// 다음과 같은 모양으로 출력되게 코드를 작성하세요.
 	// 1 2 3 4 5 6 7 8 9 10
 	// 2 3 4 5 6 7 8 9 10 1
@@ -13,4 +13,23 @@ public class Test01 {
 	// 9 10 1 2 3 4 5 6 7 8
 	// 10 1 2 3 4 5 6 7 8 9
 	
+}
+
+public class Test01 {
+
+    static public void main(String[] args) {
+
+        int num = 10;
+        for (int i = 1; i <= num; i++) { // 1. num의 수 만큼 열을 반복합니다.
+
+            for (int j = i; j <= num; j++) { // 2 - 1. 열의 값 부터 시작하는 for문. num의 수가 될때까지 반복
+                System.out.print(j+" ");
+            }
+
+            for (int k = 1; k < i; k++) { // // 2 - 2. 부족한 수를 채우는 for문
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
 }
