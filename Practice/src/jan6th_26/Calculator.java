36d19b80c78527ca49cc905246a021f356ac2933
LEE-PABLO <leepaoula01@gmail.com>
2026-01-06 12:20:14 +0900

    0106

---
+++ Practice/src/jan6th_26/Calculator.java
@@ -0,0 +1,12 @@
+package jan6th_26;
+
+public class Calculator {
+	void powerOn() {
+		System.out.println("전원을 켭니다.");
+	}
+	int plus(int a, int b) {return a+b;}
+	double divide(int a, int b) {return (double)a/b;}
+	void powerOff() {
+		System.out.println("전원을 끕니다.");
+	}
+}
