a95b1ba1e53fd3494690429c6c944bc0981da2ba
ChoHanEarl <159342404+ChoHanEarl@users.noreply.github.com>
2026-01-06 16:32:48 +0900

    20260106 answer

--- CalculatorExample.java
+++ CalculatorExample.java
@@ -1,9 +1,8 @@
-package _20260106;
+package practice05;
 
 public class CalculatorExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Calculator myCalc = new Calculator();
 		myCalc.powerOn();
 		
@@ -18,11 +17,11 @@ public class CalculatorExample {
 		myCalc.powerOff();
 		
 	}
-	//전원을 켭니다.
-	//result1: 11
-	//result2: 2.5
-	//전원을 끕니다
 	
-	//위의 결과가 나오도록 해주시면 됩니다.
-
+	// 전원을 켭니다.
+	// result1: 11
+	// result2: 2.5
+	// 전원을 끕니다
+	
+	// 위의 결과가 나오도록 해주시면 됩니다.
 }
