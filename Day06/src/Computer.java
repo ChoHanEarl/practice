ee997e3957feffb04f46d915b01da90e0a9c900d
kjg010829@naver.com <jarijarinojari@gmail.com>
2026-01-07 20:37:18 +0900

    commit

---
+++ Day06/src/Computer.java
@@ -0,0 +1,21 @@
+
+public class Computer {
+
+	public int sum1(int[] arr) {
+		int z = 0;
+		for (int i = 0; i < arr.length; i++) {
+			z += arr[i];
+		}
+		return z;
+	}
+
+	public int sum2(int a, int b, int c) {
+		return a+b+c;
+	}
+
+	
+
+	public int sum2(int a, int b, int c, int d, int e){
+		return a+b+c+d+e;
+	}
+}
