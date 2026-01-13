9777757f9cc41b08df8324978cabd86ac633b551
ChoHanEarl <159342404+ChoHanEarl@users.noreply.github.com>
2026-01-13 11:29:57 +0900

    20260112 해답

---
+++ _20260112/Animal.java
@@ -0,0 +1,10 @@
+package _20260112;
+
+public abstract class Animal {
+	String name;
+	
+	void sound() {
+		
+	}
+}
+
---
+++ _20260112/Cat.java
@@ -0,0 +1,13 @@
+package _20260112;
+
+public class Cat extends Animal {
+	
+	Cat(String name) {
+		this.name=name;
+	}
+	
+	void sound() {
+		System.out.println("야용");
+	}
+}
+
