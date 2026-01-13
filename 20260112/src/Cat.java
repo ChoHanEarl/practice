3ca26586f33d79370ed8a2de29bf2aab50af1c75
jarijarinojari-ui <jarijarinojari@gmail.com>
2026-01-13 20:28:50 +0900

    commit

---
+++ 20260112/src/Cat.java
@@ -0,0 +1,10 @@
+
+public class Cat extends Animal{
+	
+	Cat (String name) {
+		this.name = name;
+	}
+	public void sound() {
+		System.out.println("야옹");
+	}
+}
---
+++ 20260112/src/Dog.java
@@ -0,0 +1,10 @@
+
+public class Dog extends Animal{
+	
+	Dog (String name) {
+		this.name = name;
+	}
+	public void sound() {
+		System.out.println("멍멍");
+	}
+}
